c67a91961445029f34efa66dfdf39eb069a419e9
Sxyyyyyy <suxingyu1004@gmail.com>
2022-09-25 21:32:32 +0800

    update 0001

---
+++ array/0001.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = m.find(target - nums[i]);
+            if (it != m.end())
+            {
+                return {it->second, i};
+            }
+            else
+            {
+                // m.insert({nums[i], i});
+                m[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
