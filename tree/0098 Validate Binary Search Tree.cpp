8114282990b14f0fe9602f7a13117f8c68fb4c97
Suxyuuu <suxingyu1004@gmail.com>
2022-12-27 11:08:12 +0800

    AC 0098

---
+++ tree/0098 Validate Binary Search Tree.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isValidBST(TreeNode *root)
+    {
+        return help(root, nullptr, nullptr);
+    }
+
+    bool help(TreeNode *node, TreeNode *min, TreeNode *max)
+    {
+        if (node == nullptr)
+        {
+            return true;
+        }
+        if (min != nullptr && node->val <= min->val)
+        {
+            return false;
+        }
+        if (max != nullptr && node->val >= max->val)
+        {
+            return false;
+        }
+        return help(node->left, min, node) && help(node->right, node, max);
+    }
+};
+// @lc code=end
