ceeeaf858e7c2611b6c37f1edd47bd7ffc9312d7
Suxyuuu <suxingyu1004@gmail.com>
2023-02-03 10:30:12 +0800

    AC 0343

---
+++ Dynamic Programming/0343 Integer Break.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=cpp
+ *
+ * [343] Integer Break
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int integerBreak(int n)
+    {
+        vector<int> dp(n + 1);
+        for (int i = 2; i <= n; i++)
+        {
+            int curMax = 0;
+            for (int j = 1; j < i; j++)
+            {
+                curMax = max(curMax, max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
