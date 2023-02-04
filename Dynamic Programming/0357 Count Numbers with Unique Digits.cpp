68ce3f39458a3167fa146ba3ddba8ebbfb6135d2
Suxyuuu <suxingyu1004@gmail.com>
2023-02-04 22:03:11 +0800

    AC 0357

---
+++ Dynamic Programming/0357 Count Numbers with Unique Digits.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=357 lang=cpp
+ *
+ * [357] Count Numbers with Unique Digits
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int countNumbersWithUniqueDigits(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        if (n == 1)
+        {
+            return 10;
+        }
+        int ans = 10, cur = 9;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            cur *= 9 - i;
+            ans += cur;
+        }
+        return ans;
+    }
+};
+
+// @lc code=end
