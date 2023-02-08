1ea68be1fee5f06e8ae91ddc9af4c735c210fcd5
Suxyuuu <suxingyu1004@gmail.com>
2023-02-08 22:52:24 +0800

    AC 0377

---
+++ Dynamic Programming/0377 Combination Sum IV.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=377 lang=cpp
+ *
+ * [377] Combination Sum IV
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int& num : nums) {
+                if (num <= i && dp[i - num] < INT_MAX - dp[i]) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+// @lc code=end
+
