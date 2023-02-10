7f5e78cf230ec9dd756439cee4ef8682a3f2a4d4
Suxyuuu <suxingyu1004@gmail.com>
2023-02-10 14:04:39 +0800

    AC 0022

---
+++ BackTracking/0022 Generate Parentheses.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        if(n==0) return {};
+        string temp;
+        vector<string> ans;
+        dfs(n,n,temp, ans);
+        return ans;
+    }
+
+    void dfs(int lnum, int rnum, string temp, vector<string> & ans)
+    {
+        if(lnum==rnum && lnum==0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        else{
+            if(lnum>0)
+            {
+                temp.push_back('(');
+                dfs(lnum-1, rnum, temp, ans);
+                temp.pop_back();
+            }
+            if(rnum>0 && lnum<rnum)
+            {
+                temp.push_back(')');
+                dfs(lnum, rnum-1, temp, ans);
+                temp.pop_back();
+            }
+        }
+    }
+};
+// @lc code=end
+
